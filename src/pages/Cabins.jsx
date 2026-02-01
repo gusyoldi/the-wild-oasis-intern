d77afb20c269e775daf7eed026001885b1ca3dca
Gus <yoldi.gustavo@gmail.com>
2026-02-01 16:11:23 -0300

    feat(cabins): update Cabins page

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,8 @@
-import CabinTable from "../features/cabins/CabinTable";
+import AddCabin from "../features/cabins/AddCabin";
+import CabinTable from "../features/cabins/CabinTable-v2";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import AddCabin from "../features/cabins/AddCabin";
-import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (

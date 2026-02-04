3b8fe06130a0c1ebfa19869ae96b81f7c5006812
Gus <yoldi.gustavo@gmail.com>
2026-02-04 17:51:02 -0300

    fix(styled): use transient props for active, type, columns to avoid console warnings
    fix(eslint): disable fast-refresh warning for context file

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -49,15 +49,15 @@ const variations = {
 };
 
 const Button = styled.button.attrs((props) => ({
-  variation: props.variation ?? "primary",
-  size: props.size ?? "medium",
+  $variation: props.variation ?? "primary",
+  $size: props.size ?? "medium",
 }))`
   border: none;
   border-radius: var(--border-radius-sm);
   box-shadow: var(--shadow-sm);
 
-  ${(props) => sizes[props.size]}
-  ${(props) => variations[props.variation]}
+  ${(props) => sizes[props.$size]}
+  ${(props) => variations[props.$variation]}
 `;
 
 export default Button;

18cfa043b5f3bf177712ee582fdc51112abe7ead
Gus <yoldi.gustavo@gmail.com>
2026-04-21 15:26:38 -0300

    feat: make project public

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("gus@example.com");
+  const [password, setPassword] = useState("Admin123");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
